63d3fd4bd905f8b1387d98c0a14159979fdd7560
Guilhem Lettron <guilhem@lettron.fr>
2012-12-26 16:22:00 +0100

    add ability to set more specific options
    I use it for LDAP, for example

--- attributes/default.rb
+++ attributes/default.rb
@@ -32,3 +32,5 @@ default['sonar']['java_maxpermsize']       = "128m"
 default['sonar']['java_command']           = "java"
 default['sonar']['logfile_maxsize']        = "0"
 default['sonar']['syslog_loglevel']        = "NONE"
+
+default['sonar']['options']                = {}
